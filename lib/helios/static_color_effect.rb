185b7ca029c9bbaa6f0492958926a793a1dacf85
Jesse Dearing <jesse.dearing@gmail.com>
2013-05-20 11:52:41 -0700

    Adds static color effect

---
+++ lib/helios/static_color_effect.rb
@@ -0,0 +1,27 @@
+module Helios
+  class StaticColorEffect
+    def initialize(args)
+      @red = args['r'].to_i
+      @green = args['g'].to_i
+      @blue = args['b'].to_i
+      @lights = get_lights(args['lights'])
+    end
+
+    def change!
+      @lights.each do |light|
+        Lights[light] = [@red, @green, @blue]
+      end
+    end
+
+    private
+    def get_lights(value)
+      if value.is_a?(Array)
+        if value[1] == '..'
+          return (value[0].to_i..value[2].to_i)
+        end
+        return value.map(&:to_i)
+      end
+      return value
+    end
+  end
+end
