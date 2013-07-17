3092433f0b7051dd40f6c599eba2236c208e57f7
Jesse Dearing <jesse.dearing@gmail.com>
2013-07-17 16:18:49 -0700

    Refactor index method

--- lib/helios/lights.rb
+++ lib/helios/lights.rb
@@ -17,15 +17,22 @@ module Helios
       # Lights[3] = [255, 0, 0] =>
       #      south_dmx[6, 7, 8] = [255, 0, 0]
       def []=(idx, color_array)
-        idx = ((idx - 1) * 3)
-        if (1..SOUTH_SIDE_MAX).include?(idx)
-          values = idx..(idx+2)
-          south_dmx[values] = color_array 
-        else
-          idx = idx - SOUTH_SIDE_MAX
-          values = idx..(idx+2)
-          north_dmx[values] = color_array 
+        if idx.is_a? Range
+          idx.to_a.each do |i|
+            self[i] = color_array
+          end
+          return
         end
+        if 1..SOUTH_SIDE_MAX.include?(idx)
+          idx -= 1
+          dmx = north_dmx
+        else
+          idx -= (SOUTH_SIDE_MAX + 1)
+          dmx = south_dmx
+        end
+
+        range = idx..idx+2
+        dmx[range] = color_array
       end
 
       def south_dmx
