910321fc1ece38c4bbab1ee562e965e038da76f1
Jesse Dearing <jesse.dearing@gmail.com>
2013-10-11 17:40:23 -0700

    Update init.rb to use bundler and load all libs

--- init.rb
+++ init.rb
@@ -1,6 +1,11 @@
+require 'bundler'
+Bundler.setup
+
 $LOAD_PATH.concat([
   File.expand_path('../lib', __FILE__),
   File.expand_path('../vendor/rdmx', __FILE__)
 ])
 
+
 require 'helios'
+require 'lib/helios/static_color_effect'
