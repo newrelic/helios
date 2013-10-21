469ff8402d73c878d655cb817b19f12b80d1f8b0
Jesse Dearing <jesse.dearing@gmail.com>
2013-10-21 14:02:11 -0700

    Make JSON endpoint inaccessible to outside world

--- lib/helios/rack/simple_client_service.rb
+++ lib/helios/rack/simple_client_service.rb
@@ -3,6 +3,7 @@ require 'aws-sdk'
 module Helios::Rack
   class SimpleClientService
     def self.call(env)
+      return [401, {'Content-Type' => 'text/plain'}, ["Sorry. You must be at New Relic to use this."]] unless env['REMOTE_ADDR'] =~ /127\.0\.0\.1|172\.16\..*/
       begin
         json = env['rack.input'].read
         sqs = AWS::SQS.new(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'])
