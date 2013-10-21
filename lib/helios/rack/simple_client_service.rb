7e646bf1e40a1e3b37ff8552a92259ccbdf972a7
Jesse Dearing <jesse.dearing@gmail.com>
2013-10-21 15:29:28 -0700

    Use environment variable for regex whitelist

--- lib/helios/rack/simple_client_service.rb
+++ lib/helios/rack/simple_client_service.rb
@@ -3,14 +3,17 @@ require 'aws-sdk'
 module Helios::Rack
   class SimpleClientService
     def self.call(env)
-      return [401, {'Content-Type' => 'text/plain'}, ["Sorry. You must be at New Relic to use this."]] unless env['REMOTE_ADDR'] =~ /127\.0\.0\.1|172\.16\..*/
-      begin
-        json = env['rack.input'].read
-        sqs = AWS::SQS.new(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'])
-        sqs.queues.create('helios').send_message(json)
-        [200, {'Content-Type' => 'text/plain'}, ["Sent to SQS\n"]]
-      rescue Exception => ex
-        [500, {'Content-Type' => 'text/plain'}, ["There was an error\n", ex.message, "\n"]]
+      if env['REMOTE_ADDR'] =~ /127\.0\.0\.1/ || (ENV['WHITE_LIST_REGEXP'] && env['HTTP_X_FORWARDED_FOR'] =~ Regexp.new(ENV['WHITE_LIST_REGEXP']))
+        begin
+          json = env['rack.input'].read
+          sqs = AWS::SQS.new(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'])
+          sqs.queues.create('helios').send_message(json)
+          [200, {'Content-Type' => 'text/plain'}, ["Sent to SQS\n"]]
+        rescue Exception => ex
+          [500, {'Content-Type' => 'text/plain'}, ["There was an error\n", ex.message, "\n"]]
+        end
+      else
+        return [401, {'Content-Type' => 'text/plain'}, ["Sorry. You must be at New Relic to use this."]] 
       end
     end
   end
