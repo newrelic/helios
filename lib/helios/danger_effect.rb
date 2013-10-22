892a87e5ec2e63a5af0db883a7c5d81ddeb4cc33
Bryan Stearns <bryan@newrelic.com>
2013-10-21 19:21:48 -0700

    fix typo

--- lib/helios/danger_effect.rb
+++ lib/helios/danger_effect.rb
@@ -1,7 +1,7 @@
 module Helios
   class DangerEffect < Effect
     def initialize(args)
-      @iterations = args('iterations', 100]
+      @iterations = args.fetch('iterations', 100)
     end
 
     def change!
