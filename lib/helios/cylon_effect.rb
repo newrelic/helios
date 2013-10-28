c547c17df3777ecc633f52c7f43d5635d8b24d9b
Joe Lepper <jlepper@newrelic.com>
2013-10-28 13:54:45 -0700

    reverts all of my experimenting

--- lib/helios/cylon_effect.rb
+++ lib/helios/cylon_effect.rb
@@ -4,28 +4,27 @@ module Helios
     BLACK = [0,0,0]
 
     def initialize(args = {})
-      lights = args.fetch('lights', [5, '..', 8])
+      lights = args.fetch('lights', [1, '..', 8])
       @lights = get_lights(lights)
     end
 
-    # def change!
-    #   size = 2
-    #   # size = @lights.last - @lights.first
-    #   cylon_lights = Array.new(size, BLACK)
-    #   cylon_lights[0] = RED
+    def change!
+      size = @lights.last - @lights.first
+      cylon_lights = Array.new(size, BLACK)
+      cylon_lights[0] = RED
 
-    #   size.times do
-    #     set_lights(cylon_lights)
-    #     cylon_lights.rotate!(-1)
-    #     pause
-    #   end
+      size.times do
+        set_lights(cylon_lights)
+        cylon_lights.rotate!(-1)
+        pause
+      end
 
-    #   size.times do
-    #     cylon_lights.rotate!
-    #     set_lights(cylon_lights)
-    #     pause
-    #   end
-    # end
+      size.times do
+        cylon_lights.rotate!
+        set_lights(cylon_lights)
+        pause
+      end
+    end
 
     def pause
     end
