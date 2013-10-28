9f39e1979c53ca21841fd9b7d831c4be90a59b3e
Jesse Dearing <jesse.dearing@gmail.com>
2013-10-28 13:39:52 -0700

    Add HTTP basic auth

--- lib/helios/rack/simple_client_service.rb
+++ lib/helios/rack/simple_client_service.rb
@@ -3,11 +3,10 @@ require 'aws-sdk'
 module Helios::Rack
   class SimpleClientService
     def self.call(env)
-      if env['REMOTE_ADDR'] =~ /127\.0\.0\.1/ || (ENV['WHITE_LIST_REGEXP'] && env['HTTP_X_FORWARDED_FOR'] =~ Regexp.new(ENV['WHITE_LIST_REGEXP']))
+      if authorized? || basic_http_authorized?(env)
         begin
           json = env['rack.input'].read
-          sqs = AWS::SQS.new(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'])
-          sqs.queues.create('helios').send_message(json)
+          post_message_to_sqs(json)
           [200, {'Content-Type' => 'text/plain'}, ["Sent to SQS\n"]]
         rescue Exception => ex
           [500, {'Content-Type' => 'text/plain'}, ["There was an error\n", ex.message, "\n"]]
@@ -15,6 +14,22 @@ module Helios::Rack
       else
         return [401, {'Content-Type' => 'text/plain'}, ["Sorry. You must be at New Relic to use this."]] 
       end
+    end
+
+    private
+    def authorized?
+      env['REMOTE_ADDR'] =~ /127\.0\.0\.1/ || (ENV['WHITE_LIST_REGEXP'] && env['HTTP_X_FORWARDED_FOR'] =~ Regexp.new(ENV['WHITE_LIST_REGEXP']))
+    end
+
+    def basic_http_authorized?(env)
+      auth = Rack::Auth::Basic::Request.new(env)
+      auth.provided? && auth.basic? && auth.credentials && auth.credentials &&
+        auth.credentials == [ENV['AUTH_USER'], ENV['AUTH_PASSWORD']]
+    end
+
+    def post_message_to_sqs(json)
+      sqs = AWS::SQS.new(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'])
+      sqs.queues.create('helios').send_message(json)
     end
   end
 end
