29d44023f84d9a1068d6f1f9d458059e134fa9b8
Jesse Dearing <jesse.dearing@gmail.com>
2013-10-28 12:50:15 -0700

    Write out pid when launching server

--- server.rb
+++ server.rb
@@ -2,6 +2,8 @@ require './init'
 
 listener = Helios::Listener.new(AWS::SQS.new(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY']))
 
+File.open('./helios.pid', 'w') { |f| f.write $$ }
+
 loop do
   listener.listen!
 end
