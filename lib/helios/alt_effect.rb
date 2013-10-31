0b62f503c5be26e6a562c05f874e0543dc6f39f1
Jonan Scheffler <jonanscheffler@gmail.com>
2013-10-31 16:54:27 -0700

    Fix up alt effect stepping.

--- lib/helios/alt_effect.rb
+++ lib/helios/alt_effect.rb
@@ -1,34 +1,33 @@
 module Helios
-    class AltEffect < Effect
-        def initialize(args)
-            @odd_red = args['r'].to_i
-            @odd_green = args['g'].to_i
-            @odd_blue = args['b'].to_i
-            @even_red = args['r1'].to_i
-            @even_green = args['g1'].to_i
-            @even_blue = args['b1'].to_i
-            @iterations = args['iterations']
-            @interval = args.fetch('interval', 0.05)
-            lights = args.fetch('lights', [1, '..', 25])
-            @lights = get_lights(lights)
-        end
-        
-        def change!
-            odd_color = [@odd_red, @odd_green, @odd_blue]
-            even_color = [@even_red, @even_green, @even_blue]
-            
-            @iterations.times do
-                alt_colors(odd_color, even_color)
-                sleep @interval
-            end
-        end
-        
-        def alt_colors(odd_color, even_color)
-            @lights.step(2).each do |light|
-                set_light(light, odd_color)
-                set_light(light.next, even_color)
-            end
-        end
+  class AltEffect < Effect
+    def initialize(args)
+      @odd_red = args['r'].to_i
+      @odd_green = args['g'].to_i
+      @odd_blue = args['b'].to_i
+      @even_red = args['r1'].to_i
+      @even_green = args['g1'].to_i
+      @even_blue = args['b1'].to_i
+      @iterations = args['iterations']
+      @interval = args.fetch('interval', 0.05)
+      lights = args.fetch('lights', [1, '..', 25])
+      @lights = get_lights(lights)
     end
-end
 
+    def change!
+      odd_color = [@odd_red, @odd_green, @odd_blue]
+      even_color = [@even_red, @even_green, @even_blue]
+
+      @iterations.times do
+        alt_colors(odd_color, even_color)
+        sleep @interval
+      end
+    end
+
+    def alt_colors(odd_color, even_color)
+      @lights.each do |light|
+        Lights[light] = odd_color
+        Lights[light + 1] = even_color unless @lights.last == light
+      end
+    end
+  end
+end
