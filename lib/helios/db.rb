f25805770dfade37773b79be3e15e84bf5b3c5ec
Jesse Dearing <jesse.dearing@gmail.com>
2013-11-01 16:15:39 -0700

    Store state of lights in redis and reload on boot

---
+++ lib/helios/db.rb
@@ -0,0 +1,18 @@
+module Helios
+  class DB
+    include Singleton
+
+    def set(key, value)
+      db.set(key, value)
+    end
+
+    def get(key)
+      db.get(key)
+    end
+
+    private
+    def db
+      @db ||= Redis.new(host: (ENV['REDIS_HOST'] || '127.0.0.1'))
+    end
+  end
+end
