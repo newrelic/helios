0ba448d11f841a0a0367c2e9d49d5aa390c6fe7f
Jesse Dearing <jesse.dearing@gmail.com>
2013-12-11 16:22:20 -0800

    Saving before indexes are segmented

--- lib/helios/lights.rb
+++ lib/helios/lights.rb
@@ -26,6 +26,11 @@ module Helios
           end
           return
         end
+
+        unless color_array.find {|color| color < 0}
+          Helios::DB.instance.set("helios::light::#{idx}", color_array)
+        end
+
         if (1..SOUTH_SIDE_MAX).include?(idx)
           dmx = south_dmx
         else
@@ -34,9 +39,6 @@ module Helios
         end
         min_range = ((idx-1) * 3)
         range = min_range..(min_range+2)
-        unless color_array.find {|color| color < 0}
-          Helios::DB.instance.set("helios::light::#{idx}", color_array)
-        end
 
         dmx[range] = color_array
       end
